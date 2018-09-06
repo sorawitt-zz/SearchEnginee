e22f85288137a9e408248ecb8d68694279643105
Sorawit Trutsat <sorawit.tr@gmail.com>
2018-09-06 22:05:21 +0700

    Add Algolia

---
+++ index.js
@@ -0,0 +1,82 @@
+const algoliasearch = require('algoliasearch');
+const dotenv = require('dotenv');
+const firebase = require('firebase');
+
+// load values from the .env file in this directory into process.env
+dotenv.load();
+
+// configure firebase
+firebase.initializeApp({
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+});
+const database = firebase.database();
+
+// configure algolia
+const algolia = algoliasearch(
+  process.env.ALGOLIA_APP_ID,
+  process.env.ALGOLIA_API_KEY
+);
+const index = algolia.initIndex(process.env.ALGOLIA_INDEX_NAME);
+
+// Get all contacts from Firebase
+database.ref('/school').once('value', schools => {
+  // Build an array of all records to push to Algolia
+  const records = [];
+  schools.forEach(school => {
+    // get the key and data from the snapshot
+    const childKey = school.key;
+    const childData = school.val();
+    // We set the Algolia objectID as the Firebase .key
+    childData.objectID = childKey;
+    // Add object for indexing
+    records.push(childData);
+  });
+
+  // Add or update new objects
+  index
+    .saveObjects(records)
+    .then(() => {
+      console.log('School imported into Algolia');
+    })
+    .catch(error => {
+      console.error('Error when importing school into Algolia', error);
+      process.exit(1);
+    });
+});
+
+const schoolsRef = database.ref('/school');
+schoolsRef.on('child_added', addOrUpdateIndexRecord);
+schoolsRef.on('child_changed', addOrUpdateIndexRecord);
+schoolsRef.on('child_removed', deleteIndexRecord);
+
+function addOrUpdateIndexRecord(school) {
+  // Get Firebase object
+  const record = school.val();
+  // Specify Algolia's objectID using the Firebase object key
+  record.objectID = school.key;
+  // Add or update object
+  index
+    .saveObject(record)
+    .then(() => {
+      console.log('Firebase object indexed in Algolia', record.objectID);
+    })
+    .catch(error => {
+      console.error('Error when indexing contact into Algolia', error);
+      process.exit(1);
+    });
+}
+
+function deleteIndexRecord(school) {
+  // Get Algolia's objectID from the Firebase object key
+  const objectID = school.key;
+  // Remove the object from Algolia
+  index
+    .deleteObject(objectID)
+    .then(() => {
+      console.log('Firebase object deleted from Algolia', objectID);
+    })
+    .catch(error => {
+      console.error('Error when deleting contact from Algolia', error);
+      process.exit(1);
+    });
+}
